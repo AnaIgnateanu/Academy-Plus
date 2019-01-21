e657b2d0a799888082a4a319b7bb1cc150fc113d
Ana Manolache <anamanolache@Anas-MacBook-Air.local>
2019-01-21 09:43:56 +0100

    removed old files

--- proiecte 2017-2018/libft/bzero.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void bzero(void *str, size_t n)
-{
-	ft_memset((void *)str, 0, (size_t) n);
-}
--- proiecte 2017-2018/libft/ft_bzero.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-/*Erases the data in the n bytes of the memory starting 
-at the location pointed to by str, by writting zeroes to that area.
-*/
-void	ft_bzero(void *str, size_t n)
-{
-	ft_memset((void *)str, 0, (size_t) n);
-}
